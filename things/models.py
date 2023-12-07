e1b5ec98a8fdb289c86c7914b02f2c5a50bae846
haiwei-luo <haiwei8809@gmail.com>
2023-12-07 04:05:00 +0000

    Fix error

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Thing():
+class Thing(models.Model):
     name = models.CharField(
         max_length = 20,
         blank = False, 
