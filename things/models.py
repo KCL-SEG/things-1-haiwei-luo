cad98d240f3507354decba2ce02788a2ea496c21
haiwei-luo <haiwei8809@gmail.com>
2023-12-07 04:10:19 +0000

    Specify maximum and minimum value of quantity

--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
@@ -12,4 +13,6 @@ class Thing(models.Model):
         blank = False, 
         unique = False,                
     )
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
